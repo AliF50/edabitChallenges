93b13bee691534d062ef3412f15f43abe061caa7
Ali Hassan <alihassan@post.com>
2017-10-03 17:21:38 -0400

    .

---
+++ sumTwoSmallestNums.js
@@ -0,0 +1,9 @@
+//Create a function that takes an array of numbers and
+//returns the sum of the two lowest positive integers. No floats or empty arrays will be used in any of the test cases.
+function sumTwoSmallestNums(arr) {
+  arr = arr.filter((num) =>
+    return num > 0;
+  });
+  arr = arr.sort((a, b) => a - b);
+  return arr[0] + arr[1];
+}
